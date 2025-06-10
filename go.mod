0152d183dbd9bb2db6dd491eb9bb410e01af6353
ei-sugimoto <eikun1014@gmail.com>
2025-06-11 02:24:39 +0900

    github

--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module github.com/ei-sugimoto/hayosenkai
 
 go 1.23.8
 
-require github.com/slack-go/slack v0.17.1
+require (
+	github.com/google/go-github/v72 v72.0.1-0.20250609211205-8c3cc7debd52
+	github.com/slack-go/slack v0.17.1
+	golang.org/x/oauth2 v0.30.0
+)
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 
+	"github.com/google/go-github/v72/github"
 	"github.com/slack-go/slack"
+	"golang.org/x/oauth2"
 )
 
 func main() {
+	ctx := context.Background()
 	flag.Parse()
 	args := flag.Args()
 
@@ -16,7 +20,11 @@ func main() {
 	}
 
 	slackToken := args[0]
+	githubToken := args[1]
 
 	s := slack.New(slackToken)
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: githubToken})
+	tc := oauth2.NewClient(ctx, ts)
+	gh := github.NewClient(tc)
 
 }
